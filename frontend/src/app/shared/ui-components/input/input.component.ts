ee30020fd75c055dcb16db7501abf3f266a054c0
ibg101 <ssksg101@gmail.com>
2023-08-30 15:25:00 +0200

    input v1 done

--- frontend/src/app/shared/ui-components/input/input.component.ts
+++ frontend/src/app/shared/ui-components/input/input.component.ts
@@ -8,5 +8,6 @@ import { AbstractControl, FormControl } from '@angular/forms';
 })
 export class InputComponent {
   @Input() label: string = '';
+  @Input() placeholder: string = '';
   @Input() control: FormControl = new FormControl();
 }
