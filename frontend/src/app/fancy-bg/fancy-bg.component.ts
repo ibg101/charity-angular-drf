247cf18d53cb3d921ff9275be559fffa43902a6c
ibg101 <ssksg101@gmail.com>
2023-08-31 17:34:06 +0200

    save

--- frontend/src/app/fancy-bg/fancy-bg.component.ts
+++ frontend/src/app/fancy-bg/fancy-bg.component.ts
@@ -6,31 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./fancy-bg.component.scss']
 })
 export class FancyBgComponent {
-  @Input() public position: string | string[] = [];
-
-  get top(): boolean {
-    return this.definePos('top');
-  }
-
-  get bottom(): boolean {
-    return this.definePos('bottom');
-  }
-
-  get left(): boolean {
-    return this.definePos('left');
-  }
-
-  get right(): boolean {
-    return this.definePos('right');
-  }
-
-  private definePos(pos: string): boolean {
-    if (this.position.length > 1) {
-      for (let i of this.position) {
-        return i === pos ? true : false;
-      }
-    }
-    // only two ==
-    return this.position == pos ? true : false;
-  }
+  
 }
