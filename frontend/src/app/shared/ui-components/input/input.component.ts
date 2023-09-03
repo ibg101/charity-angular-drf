099cd0dd5c9775c2cc84ea09117f20ef04db2a7a
ibg101 <ssksg101@gmail.com>
2023-09-03 18:11:43 +0200

    working on optimization

--- frontend/src/app/shared/ui-components/input/input.component.ts
+++ frontend/src/app/shared/ui-components/input/input.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],  
 })
 export class InputComponent implements OnInit {
   @Input() public label: string = '';
@@ -14,7 +15,16 @@ export class InputComponent implements OnInit {
   @Input() public control: FormControl<any> = new FormControl();
   public inputType: string = '';
 
+  constructor(
+    private auth: AuthService,
+  ) { }
+
   ngOnInit(): void {
     this.isCheckbox ? this.inputType = 'checkbox' : this.inputType = 'text'; 
+  }
+
+  toggleCheckboxState(event: Event): void {
+    const checked: boolean = (event.target as HTMLInputElement).checked;
+    this.auth.getControl('rememberMe').setValue(checked);
   }
 }
