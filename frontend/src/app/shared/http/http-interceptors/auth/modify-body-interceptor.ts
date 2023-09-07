bdeae1fdbab634c65bc4901c7a8fe633728bfaa8
ibg101 <ssksg101@gmail.com>
2023-09-07 12:49:05 +0200

    register user works

--- frontend/src/app/shared/http/http-interceptors/auth/modify-body-interceptor.ts
+++ frontend/src/app/shared/http/http-interceptors/auth/modify-body-interceptor.ts
@@ -11,14 +11,18 @@ import { AuthOnly } from "../../headers";
 
 /**
 * Excludes RememberMe property from the req.body object, since there's no need to pass this field yet
-* (current setup of backend doesn't support passing rememberMe property) 
+* (current setup of backend doesn't support passing rememberMe property).
+*
+* Renames Angular's camelCase confirmPassword to DRF's snake_case confirm_password.
 */
 @Injectable()
-export class RememberMeInterceptor implements HttpInterceptor {
+export class ModifyAuthBodyInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<IUser>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.headers.has(AuthOnly.key)) {
+      const confirm_password = req.body?.confirmPassword;
+      delete req.body?.confirmPassword; // renaming it to confirm_password
       delete req.body?.rememberMe;
-      const modifiedBody = { ...req.body };
+      const modifiedBody = { ...req.body, confirm_password: confirm_password };
       const modifiedReq = req.clone({ body: modifiedBody });
       return next.handle(modifiedReq);
     }
