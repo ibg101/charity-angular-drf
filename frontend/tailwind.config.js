c692a7899fdb65c250f4d3950e90b0c677d49e2a
ibg101 <ssksg101@gmail.com>
2023-09-30 18:04:09 +0200

    layot improvments

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,10 +8,13 @@ module.exports = {
       gradientColorStops: {
         'primary': '#845EC2',
         'secondary': '#D65DB1',
-      }
+      },
+      screens: {
+        'md-h': {'raw': '(max-height: 768px)'}
+      },
     },
   },
-  // need for applying styles to custom checkbox (without this plugin styles, applied, f.e. on checkbox, dont work)
+  // need for applying styles to custom checkbox (without this plugin, applied styles (f.e., on checkbox), dont work)
   plugins: [require("@tailwindcss/forms")],
   prefix: "tw-",
   darkMode: "class",
