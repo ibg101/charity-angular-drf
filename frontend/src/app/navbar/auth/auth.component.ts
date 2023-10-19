46ba359e155a600f358894448f5b0cc68bfc474b
ibg101 <ssksg101@gmail.com>
2023-10-19 15:47:39 +0200

    layout related fixes

--- frontend/src/app/navbar/auth/auth.component.ts
+++ frontend/src/app/navbar/auth/auth.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { LinksService } from 'src/app/shared/services/links/links.service';
-import { NavbarService } from '../services/navbar.service';
 
 @Component({
   selector: 'nav-auth',
@@ -10,7 +9,6 @@ import { NavbarService } from '../services/navbar.service';
 })
 export class AuthComponent {
   constructor(
-    public nav: NavbarService,
     public link: LinksService,
     public auth: AuthService,
   ) { }
